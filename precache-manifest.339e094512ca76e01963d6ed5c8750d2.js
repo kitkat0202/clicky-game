ccf22db666f3213706f6bc576c8b47f20fb7676c
KatHe <he.katherine321@gmail.com>
2018-10-27 11:07:24 -0400

    Updates

---
+++ precache-manifest.339e094512ca76e01963d6ed5c8750d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "f3a194edd2eee9557a91",
+    "url": "/clicky-game/static/js/main.f3a194ed.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/clicky-game/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "f3a194edd2eee9557a91",
+    "url": "/clicky-game/static/css/main.eb245b37.chunk.css"
+  },
+  {
+    "revision": "34ff9e32427f522c627a67cc0bf8ea6d",
+    "url": "/clicky-game/index.html"
+  }
+];
